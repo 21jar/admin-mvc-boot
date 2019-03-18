d6ade597c1c6e0470065716b3dc88dadb329fbb9
slt <slt680@163.com>
2019-03-18 17:07:32 +0800

    Initial commit

---
+++ src/main/java/com/ixiangliu/AdminMvcBootApplication.java
@@ -0,0 +1,13 @@
+package com.ixiangliu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminMvcBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminMvcBootApplication.class, args);
+    }
+
+}
