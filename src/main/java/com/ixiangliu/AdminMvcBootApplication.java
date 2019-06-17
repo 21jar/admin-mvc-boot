e7cbf61ed226a10752cc0c9de1c499e5915a380f
21jar <slt680@163.com>
2019-06-17 10:34:05 +0800

    数据库代码中文搜索失败

--- src/main/java/com/ixiangliu/AdminMvcBootApplication.java
+++ src/main/java/com/ixiangliu/AdminMvcBootApplication.java
@@ -1,9 +1,7 @@
 package com.ixiangliu;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
 public class AdminMvcBootApplication {
