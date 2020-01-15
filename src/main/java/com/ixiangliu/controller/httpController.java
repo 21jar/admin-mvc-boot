86f84b850f803711c46c63dea4ce9580b07dff6b
21jar <slt680@163.com>
2020-01-15 13:04:05 +0800

    获取配置文件

--- src/main/java/com/ixiangliu/controller/httpController.java
+++ src/main/java/com/ixiangliu/controller/httpController.java
@@ -1,12 +1,16 @@
 package com.ixiangliu.controller;
 
 import com.ixiangliu.common.utils.Result;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/http")
 public class httpController {
+
+//    @Value("${port}")
+//    private String port;
 
     /**
      * 列表
