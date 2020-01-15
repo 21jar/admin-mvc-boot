9b7d5da2fdd7cf438863e4f2de873b938527bbee
21jar <slt680@163.com>
2020-01-15 11:49:53 +0800

    加入返回结果类

--- src/main/java/com/ixiangliu/controller/httpController.java
+++ src/main/java/com/ixiangliu/controller/httpController.java
@@ -1,5 +1,6 @@
 package com.ixiangliu.controller;
 
+import com.ixiangliu.common.utils.Result;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,9 @@ public class httpController {
      * 列表
      */
     @RequestMapping("/list")
-    public void list(){
+    public Result list(){
         System.out.println("111");
+        return Result.ok("success");
     }
 
 }
